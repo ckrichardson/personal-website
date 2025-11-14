84b25dc426722213bb61af6cb315399bbfe71de0
ckrichardson <cliffordkrichardson@gmail.com>
2025-11-13 18:27:08 -0800

    prefetching

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
+  prefetch: true,
   adapter: cloudflare({
     imageService: "passthrough",
     platformProxy: {
